f8779a6ee2dc85b797190b5fd7bc8a6b51332ff6
Elin <982467922@qq.com>
2022-01-06 23:47:44 +0800

    Day07-Build Basic Calculator

---
+++ Day06-Getting User Input/Day06-Getting User Input/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06_Getting_User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 使用Console.Write() + Console.ReadLine()进行用户输入获取，并使用变量进行存储
+            Console.Write("Who are you?");
+            Console.O
+            string name = Console.ReadLine();
+            Console.WriteLine("Greetings " + name + " and welecome you are trying to learn C#.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day07-Build Basic Calculator/Day07-Build Basic Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Day07_Build_Basic_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 使用Convert类下的方法实现字符串向数字（反之）的转换
+            Console.Write("Input the first value : ");
+            dynamic number1 = 0;
+            dynamic number2 = 0;
+            string get_uservalue1 = Console.ReadLine();
+            try
+            {
+                number1 = Convert.ToInt32(get_uservalue1);
+            }
+            catch(Exception ex)
+            {
+                number1 = Convert.ToDouble(get_uservalue1);
+            }
+            Console.Write("Input the second value : ");
+            string get_uservalue2 = Console.ReadLine();
+            try
+            {
+                number2 = Convert.ToInt32(get_uservalue2);
+            }
+            catch (Exception ex)
+            {
+                number2 = Convert.ToDouble(get_uservalue2);
+            }
+            Console.Write("Input the method : ");
+            string method = Console.ReadLine();
+            if (method == "+")
+            {
+                Console.WriteLine("Result is {0}", (number1 + number2));
+            }
+            else if (method == "-")
+            {
+                Console.WriteLine("Result is {0}", (number1 - number2));
+            }
+            else if (method == "*")
+            {
+                Console.WriteLine("Result is {0}", (number1 * number2));
+            }
+            else if (method == "/")
+            {
+                Console.WriteLine("Result is {0}", (number1 / number2));
+            }
+            else if (method == "%")
+            {
+                Console.WriteLine("Result is {0}", (number1 % number2));
+            }
+            Console.ReadLine();
+        }
+    }
+}
