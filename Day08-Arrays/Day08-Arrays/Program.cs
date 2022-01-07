c81418a1ed0af1ca6f80e6fb1171c5f8b4d9f389
Elin <982467922@qq.com>
2022-01-07 18:47:18 +0800

    Day08-Arrays

--- Day08-Arrays/Day08-Arrays/Program.cs
+++ Day08-Arrays/Day08-Arrays/Program.cs
@@ -10,6 +10,19 @@ namespace Day08_Arrays
     {
         static void Main(string[] args)
         {
+            // 使用[]来声明一个数组
+            int[] luckynumber = {4,8,15,16,23,42};
+            for(int i = 0; i < luckynumber.Length; i++)
+            {
+                Console.WriteLine(luckynumber[i]);
+            }
+            // 创建一个空的矩阵进行备用，在new string[]中的[]可以指定这个矩阵允许承载的最大元素数量
+            string[] luckname = new string[5];
+            // 可以使用数组的索引对其进行值插入或者重新更改
+            luckname[0] = "Mike";
+            luckname[1] = "John";
+            Console.WriteLine(luckname[1]);
+            Console.ReadLine();
         }
     }
 }
