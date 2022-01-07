6539eb3478bb8a88cdff7a69efbd73709b669e43
Elin <982467922@qq.com>
2022-01-07 23:44:13 +0800

    Day10-Return Method Statment

---
+++ Day09-Methods/Day09-Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day09_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 在C#中，方法可以同Python一样直接调用，当方法存在返回值，则需要变量对其进行承接
+            SayHi("John",12);
+            Console.ReadLine();
+        }
+        // static表示该方法是一个静态方法，void表示无返回值方法
+        // 通过在()中指定有效参数
+        static void SayHi(string name,int age)
+        {
+            Console.WriteLine("Hi " + name + " and you are age : " + age);
+        }
+    }
+}
---
+++ Day10-Return Method Statment/Day10-Return Method Statment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_Return_Method_Statment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入计算边长：");
+            int side_len = Convert.ToInt32(Console.ReadLine());
+            string volume = Cubenum(side_len);
+            Console.WriteLine(volume);
+            Console.ReadLine();
+        }
+        static dynamic Cubenum(int side_len)
+        {
+            int volume = (int)Math.Pow(side_len, 3);
+            string result = "这个正方体的体积为 ：" + volume;
+            return result;
+        }
+    }
+}
