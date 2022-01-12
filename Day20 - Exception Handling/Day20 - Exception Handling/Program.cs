371d878f24f716726eb9d4eff623d9d7da0569f9
Elin <982467922@qq.com>
2022-01-12 21:40:21 +0800

    Day20-Exception Handling

---
+++ Day20 - Exception Handling/Day20 - Exception Handling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20___Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                try
+                {
+                    Console.Write("Enter a number : ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter another number : ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(num1 / num2);
+
+                    Console.ReadLine();
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("You are tring divide zero!Please Retry");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Sure you are using correct C# support Num type?");
+                }
+                finally
+                {
+                    Console.WriteLine("Expression Call Error");
+                }
+            }
+        }
+}
