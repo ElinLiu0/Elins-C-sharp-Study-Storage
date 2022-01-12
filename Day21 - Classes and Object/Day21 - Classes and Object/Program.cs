562e692b14d27e28e62d067c28c5cedf068072fe
Elin <982467922@qq.com>
2022-01-12 22:33:46 +0800

    Day21 - Classes and Object

---
+++ Day21 - Classes and Object/Day21 - Classes and Object/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21___Classes_and_Object
+{
+    // internal关键字是类型和类型成员的访问修饰符。只有在同一个程序集的文件中，内部类型或者是成员才可以访问
+    // 当class前的修饰为public时，则可被外部cs源文件进行调用
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            book book1= new book();
+            book1.Title = "Harry Potter";
+            book1.Author = "J.K.Rowling";
+            book1.pages = 128;
+            Console.WriteLine(book1);
+            Console.WriteLine("The Information of {0} are list :\nPages : {1}\nAuthor by : {2}",book1.Title,book1.pages,book1.Author);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day21 - Classes and Object/Day21 - Classes and Object/book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21___Classes_and_Object
+{
+    public class book
+    {
+        public string Title;
+        public string Author;
+        public int pages;
+    }
+}
